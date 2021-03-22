1316c2add1592810ffd60e009c62c730a851fa34
Gerson Dias <gersondias@outlook.com>
2021-03-22 17:28:02 -0300

    using new sdk version

--- examples/react-sample/src/App.tsx
+++ examples/react-sample/src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import './App.css';
 
-import { Rh24WebApp } from 'rh24-webapp-sdk';
+import { Rh24WebApp } from '@metal-heaven/rh24-webapp-sdk';
 
 function App() {
   useEffect(() => {
@@ -17,8 +17,64 @@ function App() {
       },
       theme: {
         logoSrc: process.env.REACT_APP_RH24_LOGO_SRC || '',
-        backgroundColor: 'red',
-        backgroundImage: ''
+        typography: {
+          fontFamily: ['Lato', 'sans-serif'].join(','),
+          button: {
+            textTransform: 'none' as any
+          }
+        },
+        verticalMenu: {
+          backgroundColor: 'rgba(229, 229, 229, 0.95)',
+        },
+        overrides: {
+          MuiCssBaseline: {
+            '@global': {
+              body: {
+                backgroundImage: 'none',
+                backgroundSize: 'cover'
+              }
+            }
+          },
+          MuiAppBar: {
+            colorPrimary: {
+              backgroundColor: '#434C59',
+              color: '#fafafa',
+              '& button': {
+                color: 'inherit'
+              },
+              '& .MuiInputBase-root > div': {
+                color: 'inherit'
+              },
+            },
+          },
+          MuiFormLabel: {
+            root: {
+              whiteSpace: 'nowrap' as any
+            }
+          },
+          MuiInputLabel: {
+            root: {
+              "&$focused": {
+                color: "currentColor",
+                borderColor: "black",
+              },
+              borderColor: "white",
+            },
+          }
+        },
+        palette: {
+          primary: { main: colors.blue['700'] },
+          textPrimary: { main: colors.grey },
+          background: {
+            default: '#c5c5c560',
+            paper: colors.common.white
+          }
+        },
+        props: {
+          MuiInputLabel: {
+            shrink: true,
+          }
+        },
       }
     })
 
